2969ffe60b0ef99072abc26b81db11da73b728a7
kheengz <kheengz@gmail.com>
2019-07-26 10:48:30 +0100

    created test cases for /verify

--- test/test.js
+++ test/test.js
@@ -53,3 +53,68 @@ describe("/generate Generation of OTP test cases: ", function(){
     });
 });
 
+
+describe("/verify Verify user using phoneOrEmail and otp test cases: ", function(){
+    this.afterAll(function (done) {
+        mongoose.connection.dropCollection('users');
+        done();
+    });
+
+    var user;
+    var expiredUser;
+    before(async function () {
+        user = new User({ phoneOrEmail: '0123456789' });
+        expiredUser = new User({ phoneOrEmail: '9876543210' });
+        await user.save();
+        await expiredUser.save();
+    });
+
+    it("should verify a user with otp using and phoneOrEmail", function(done){
+        const {phoneOrEmail, otp: {code}} = user;
+        request
+            .post('/verify')
+            .send({phoneOrEmail, otp: code})
+            .then((res) => {
+                res.status.should.equal(200);
+                res.text.should.equal("VERIFIED");
+                done();
+            });
+    });
+
+    it("should not verify a user with wrong otp or phoneOrEmail", function(done){
+        request
+            .post('/verify')
+            .send({phoneOrEmail: "00000000000", otp: '00000'})
+            .then((res) => {
+                res.status.should.equal(404);
+                res.text.should.equal("INVALID");
+                done();
+            });
+    });
+
+    it("should not verify a user with expired otp", function(done){
+        expiredUser.otp.code_expiration = moment().subtract(1, 'hours');
+        expiredUser.save();
+        const {phoneOrEmail, otp: {code}} = expiredUser;
+
+        request
+            .post('/verify')
+            .send({phoneOrEmail, otp: code})
+            .then((res) => {
+                res.status.should.equal(412);
+                res.text.should.equal("INVALID");
+                done();
+            });
+    });
+
+    it("should generate otp using phone number 1", function(done){
+        request
+            .post('/verify')
+            .then((res) => {
+                // console.log('res.body 2: ', res.text);
+                res.status.should.equal(412);
+                res.text.should.equal("PhoneOrEmail and OTP are required");
+                done();
+            });
+    });
+});
